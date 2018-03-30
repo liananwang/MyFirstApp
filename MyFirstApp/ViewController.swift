9c41e4ccdaf9927353f15ff1db06cbbdc0015e83
王連安 <lianan.wang@facebook.com>
2018-03-30 20:25:40 +0800

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by 王連安 on 2018/3/30.
+//  Copyright © 2018年 lianan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
